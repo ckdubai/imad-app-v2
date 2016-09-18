12110ca62537c795118127ef8c423d516f918310
ckdubai <mailit4me@gmail.com>
2016-09-18 15:15:55 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ function createTemplate(data){
     var heading=data.heading;
     var date=data.date;
     var content= data.content;
+    var htmlTemplate=
           `<html>
             <head>
                 <title>${title}</title>

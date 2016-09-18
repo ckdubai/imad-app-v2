2fc29047d2599f29438af3c7650174cb99817da9
ckdubai <mailit4me@gmail.com>
2016-09-18 14:57:38 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content ={
+    title:'Mohmmed Anish article',
+    heading:'My First Article',
+    date:'Sep 25 2016',
+    content: `<div>
+            <p>
+                My first Article is published here...  My first Article is published here...    My first Article is published here...
+            </p>
+            <p>
+                My first Article is published here...  My first Article is published here...    My first Article is published here...
+            </p>
+            <p>
+                My first Article is published here...  My first Article is published here...    My first Article is published here...
+            </p>
+        </div>`
+    
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

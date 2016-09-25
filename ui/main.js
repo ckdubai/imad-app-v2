e321d0ba8382cfaada7fc4d1ca24d8c02381190b
ckdubai <mailit4me@gmail.com>
2016-09-25 13:44:58 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,3 @@
-/*var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function () {
-    
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHtml=counter.toString();
-    document.getElementById('count').innerText=counter;
-    
-};*/
  var button = document.getElementById('counter');
  
  button.onclick = function (){

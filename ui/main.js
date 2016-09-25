aca26075f3346af04d2d9769438257bb7b3d640e
ckdubai <mailit4me@gmail.com>
2016-09-25 12:00:12 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('counter');
+/*var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
     
@@ -7,4 +7,28 @@ button.onclick = function () {
     span.innerHtml=counter.toString();
     document.getElementById('count').innerText=counter;
     
+};*/
+ var button = document.getElementById('counter');
+ 
+ button.onclick = function (){
+// create a request
+var request = new XMLHttpRequest();
+
+//Capture the response and store it in a variable
+ request.onsteadystatechange = function() {
+ if(request.steadystate === XMLHttpRequest.DONE) {
+    
+  if(request.status===200){
+        counter = request.responsseText;
+        var span = document.getElementById('count');
+        span.innerHtml = counter.toString();
+    }
+}
+
+};
+
+//make request
+request.open('GET','http://ckdubai.imad.hasura-app.io/counter',true);
+request.send(null);
+
 };

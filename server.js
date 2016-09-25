ffd62be9ab372cf2db5ea55354e5ebe0c369b253
ckdubai <mailit4me@gmail.com>
2016-09-25 11:28:05 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/ui/main.js', function (req, res) {

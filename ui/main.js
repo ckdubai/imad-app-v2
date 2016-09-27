596b567604e85ce31576052517afe5144b576610
ckdubai <mailit4me@gmail.com>
2016-09-27 15:46:30 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,19 +24,34 @@ request.send(null);
 };
 
 //Submit button 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('btn_submit');
 
-//Make a request to the server and send names
 submit.onclick = function(){
-    var names= ['name1','name2','name3','name4'];
+// create a request
+var request = new XMLHttpRequest();
+
+//Capture the response and store it in a variable
+ request.onreadystatechange = function() {
+ if(request.readyState === XMLHttpRequest.DONE) {
+    
+  if(request.status===200){
+      
+//Make a request to the server and send names
+
+    var names =request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for(var i=0; i<names.length; i++) {
         list+= '<li>' + names[i] + '</li>';
                                       }
   document.getElementById('namelist').innerHTML=list;
-  
-    
+  }
+  }  
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://ckdubai.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
 
+};

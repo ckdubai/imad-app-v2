e2f85969af4facdd4d92ec341e0373b777c4ce20
ckdubai <mailit4me@gmail.com>
2016-10-26 10:48:06 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/submit-name',function(req,res) {
 
 app.get('/articles/:articleName', function (req, res) {
     //var articleName= req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title="+req.params.articlName,function(err,result){
+    pool.query("SELECT * FROM article WHERE title = "+req.params.articlName,function(err,result) {
         
         if(err){
             res.status(500).send(err.toString());
@@ -141,7 +141,7 @@ app.get('/articles/:articleName', function (req, res) {
                 res.status(404).send('Article not Found');
             }else {
                 
-                var articleData = results.rows[0];
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
             

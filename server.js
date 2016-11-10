979e607b76002933de9218611b06755306c25609
ckdubai <mailit4me@gmail.com>
2016-11-10 14:53:03 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -170,11 +170,18 @@ app.post('/login',function(req,res){
 });
 
 app.get('/login-check',function(req,res){
-    if(req.session && req.session.auth && req.session.auth.userId){
-        res.send('You are logged-in: '+ req.session.auth.userId.toString());
+    if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
            } else {
-               res.send('You are not logged in');
+              res.send(result.rows[0].username);    
            }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 
 app.get('/logout',function(req,res){

a37dc4708f2b2675d889e999088493ed0956475c
ckdubai <mailit4me@gmail.com>
2016-11-21 00:22:38 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -103,13 +103,14 @@ request.send(JSON.stringify({username:username,password:password}));
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
+        if(username && password) {
         console.log(username);
         console.log(password);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
-    
+        }
     };
 
 }
